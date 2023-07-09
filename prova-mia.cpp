838f646e285f3d7ad5d0b10910af8d0e4f5f2edc
AleSaccoExe <alessandrosacco002@gmail.com>
2023-07-09 21:21:35 +0200

    pronto per cominciare

--- prova-mia.cpp
+++ prova-mia.cpp
@@ -28,23 +28,39 @@ int main(int argc, char ** argv)
         std::cout<<"Bisogna inserire -t o --type\n";
         return 0;
     }
-    std::string type = gp.follow("data", 2, "-t", "--type");
+    std::string type = gp.follow("g", 2, "-t", "--type");
     std::string file_dati = gp.follow("dati.txt", 2, "-f", "--file");
     std::string file_output = gp.follow("dati-pronti.txt", 2, "-o", "--output");
 
     std::ofstream gnuplot_script("gnuplot_script");
     gnuplot_script << "set hidden3d\n";
-    gnuplot_script << "splot \"" << "dati-pronti.txt" << "\" with lines\n";
+    gnuplot_script << "splot \"" << file_output << "\" with lines\n";
     gnuplot_script<<"pause -1";
     gnuplot_script.close();
 
     std::vector<std::array<double, 3>> vertici;
     std::vector<std::array<unsigned, 3>> triangoli;
-    std::ifstream file("prova.txt");
-    std::ofstream dump_file("dati-pronti.txt");
+
+    std::ifstream file(file_dati);
+    std::ofstream dump_file(file_output);
+
     std::string line;
     getline(file, line);
-    for(unsigned i = 0; i<150 ; ++i)
+    std::string s_num_elem,s_num_ver, num_data;
+    std::istringstream ss(line);
+    unsigned num_elem, num_ver;
+    ss>>num_ver;
+    ss>>num_elem;
+    std::cout<<"numero di vertici: "<<num_ver<<", numero elementi: "<<num_elem<<std::endl;
+    dump_file<<num_elem<<std::endl;
+    // unsigned num_ver = std::stoi(s_num_ver);
+   
+    // unsigned num_elem = std::stoi(s_num_elem);
+ 
+
+    vertici.reserve(num_ver);
+    
+    for(unsigned i = 0; i<num_ver ; ++i)
     {
         getline(file, line);
         std::string coordinata;
@@ -57,7 +73,9 @@ int main(int argc, char ** argv)
         }
 
     }
-    for(unsigned i = 0; i< 296; ++i)
+
+    triangoli.reserve(num_elem);
+    for(unsigned i = 0; i< num_elem; ++i)
     {
         getline(file, line);
         std::string num_vertice;
@@ -81,6 +99,22 @@ int main(int argc, char ** argv)
             dump_file<<std::endl;
         }
         dump_file<<std::endl;
+    }
+    if(type == "dg")
+    {
+        while(getline(file, line))
+        {
+            std::istringstream ss(line);
+            std::string coordinata;
+            ss>>coordinata;
+            for (unsigned i = 0; i < 3; i++)
+            {
+                ss>>coordinata;
+                dump_file<<coordinata<<" ";
+            }
+            dump_file<<std::endl;
+            
+        }
     }
     file.close();
     dump_file.close();
